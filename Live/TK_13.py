88d929f6b25951da56ed2a80fd688451fbacee09
Pol24rus <poladko.dv@roslesinforg.ru>
2024-05-24 14:25:43 +0700

    2024_05_24 add file in comment

---
+++ Live/TK_13.py
@@ -0,0 +1,79 @@
+# добавить файлв комментарий
+import self
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import Keys
+# import datetime
+from datetime import datetime
+
+link = "https://intranet-test.roslesinforg.ru/stream/"
+
+try:
+    driver = webdriver.Chrome()
+    driver.get(link)
+
+    # авторизация
+    login = driver.find_element(By.ID, 'USER_LOGIN')
+    login.send_keys("OK_RLI")
+    password = driver.find_element(By.ID, "USER_PASSWORD")
+    password.send_keys("OK_RLIOK_RLI")
+    button1 = driver.find_element(By.CSS_SELECTOR, "input.btn")
+    button1.click()
+    # time.sleep(1)
+    """данный кейс ставить Желательно после ТК_5 (редактирование сообщения), чтобы находить и редактировать сообщение, а не задачи и тд."""
+    """Для редактирования необходимо открывать фрейм (?) """
+    # ввожу дату и время, чтобы получить уникальность сообщения
+    input_txt = datetime.now()
+    # TK_13 (add file in comment)
+    # нахожу поле комментария, кликаю сразу в него, не нажимая кнопку Коментировать
+    comment_field = driver.find_element(By.XPATH, "(//a[@class='feed-com-add-link'])[1]")
+    comment_field.click()
+    comment_field.send_keys("Adding file")  # не работает. Фрейм?
+    comment_field.send_keys(Keys.RETURN)
+    # (//a[@class="feed-com-add-link"])[1]
+    # original_text = driver.find_element(By.XPATH, '(//div[@class="feed-post-right-top-corner"]/div)[1]')
+    # original_text.click()
+    # time.sleep(1)
+    # # original_text.send_keys(Keys.DOWN)  # не работает
+    # # original_text.send_keys(Keys.ARROW_DOWN)
+    # edit_field = driver.find_element(By.XPATH, '//div[@class="popup-window"]/div/div/div/a[2]')
+    # time.sleep(2)
+    # edit_field.click()
+    #
+    # iframe1: WebElement = driver.find_element(By.CSS_SELECTOR,
+    #                                           "#bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm."
+    #                                           "bxhtmled-iframe-cnt > iframe")
+    # driver.switch_to.frame(iframe1)
+    # input5 = driver.find_element(By.TAG_NAME, "body")
+    # input5.send_keys("Edited ")
+    # input5_1 = input5.text
+    # input5.send_keys(Keys.CONTROL + Keys.ENTER)
+    # driver.switch_to.default_content()
+    # # нажимаю кнопку Отправить, заменил поиск кнопки нажатием Ctrl+Enter выше
+    # # send_key = (driver.find_element(By.ID, 'blog-submit-button-save'))
+    # # send_key.click()
+    #
+    # """ создам переменную, буду искать новый, отредактированный текст по части предложения. Это будет actual_text
+    # # contains(text() - метод поиска по части текста
+    # # пример # user_name = driver.find_element(By.XPATH, "//h4[contains(text(), 'Password for all ')]")
+    # # требуемый текст, как описать только часть требуемого текста?"""
+    #
+    # time.sleep(2)
+    # actual_text = driver.find_element(By.XPATH, "//div[contains(text(), 'Edited')]").text
+    # # print("input5_1 - ", input5_1)
+    # # print("actual_text - ", actual_text)
+    # # (//div[@class="feed-post-text"])[1]
+    # assert actual_text == input5_1
+
+
+
+finally:
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    driver.quit()
+
+# не забываем оставить пустую строку в конце файла
